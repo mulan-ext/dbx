382e5de45b073a2b781c1afb0e7b6cc4d537ad6d
陌竹 <mozhu233@outlook.com>
2025-10-25 06:20:48 +0800

    🎉 Initializing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mulan Ext DBX (GORM)
+
+根据配置 DSN Schema 使用不同数据库
+
+## 支持
+
+- [x] PostgreSQL
+- [x] MySQL
+- [x] SQLite (Pure-Go)
---
+++ dbx.go
@@ -0,0 +1,43 @@
+package dbx
+
+import (
+	"errors"
+	"net/url"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/virzz/mulan/db"
+)
+
+func New(cfg *db.Config) (*gorm.DB, error) {
+	_dsn, err := url.Parse(cfg.DSN)
+	if err != nil {
+		return nil, err
+	}
+	gormCfg := &gorm.Config{
+		Logger: db.NewLogger(cfg.Debug),
+	}
+	var dialector gorm.Dialector
+	switch _dsn.Scheme {
+	case "mysql":
+		dialector = mysql.New(mysql.Config{
+			DSN:                    cfg.String(),
+			DefaultStringSize:      255,
+			DontSupportRenameIndex: true,
+		})
+		return db.New(dialector, cfg.Conn, gormCfg)
+	case "postgres":
+		dialector = postgres.New(postgres.Config{
+			DSN:                  cfg.String(),
+			PreferSimpleProtocol: true,
+		})
+	case "sqlite":
+		dialector = sqlite.Open(cfg.String())
+	default:
+		return nil, errors.New("unsupported scheme: " + _dsn.Scheme)
+	}
+	return db.New(dialector, cfg.Conn, gormCfg)
+}
